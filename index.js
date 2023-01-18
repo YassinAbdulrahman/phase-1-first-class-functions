80d4f4b3dae5c64ceb81671391ee3bcb035f1bc4
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-01-18 18:17:54 +0300

    Done Work

---
+++ index.js
@@ -0,0 +1,23 @@
+function receivesAFunction(spy) {
+    return spy();
+}
+
+console.log(receivesAFunction(function () { return 'cool'}))
+
+function returnsANamedFunction (){
+    let fn = function cool(){
+        console.log("Hello");
+    }
+
+    return fn
+}
+
+console.log(returnsANamedFunction())
+
+function returnsAnAnonymousFunction(){
+    return function (){
+        console.log('Kool');
+    }
+
+}
+console.log(returnsAnAnonymousFunction())
